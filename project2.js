9b94838bd206d1dafeac9f78318a7df6c3eee76e
Andrzej_Janulewicz <and.yanulevich@gmail.com>
2025-03-21 17:20:24 +0100

    new project1

--- project2.js
+++ project2.js
@@ -1,14 +1,17 @@
 function word(a, b){
+    if(b.length > a.lengnt){
+         return false;
+    }
     for(let i = b.length - 1; i >= 0; i--){
         if(a[i] !== b[i]){
-        return(false)
+        return(false);
         }
         return(true);
 }
 }
 
 function number(a){
-    for(let i = a;i >= 0; i--)
+    for(let i = a; i >= 0; i--)
 }
 
 console.log(number(10));
