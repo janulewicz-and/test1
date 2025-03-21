60a645cf5c5a75970607d9fa10f7f9100470fc9a
Andrzej_Janulewicz <and.yanulevich@gmail.com>
2025-03-21 17:19:15 +0100

    new project

--- myproject.js
+++ myproject.js
@@ -16,7 +16,7 @@ function pow(b) {
     let result = 1;
     for (let i = 1; i < b; i++) {
         result *= i;
-}
+    }
     return result
 }
 
---
+++ project2.js
@@ -0,0 +1,14 @@
+function word(a, b){
+    for(let i = b.length - 1; i >= 0; i--){
+        if(a[i] !== b[i]){
+        return(false)
+        }
+        return(true);
+}
+}
+
+function number(a){
+    for(let i = a;i >= 0; i--)
+}
+
+console.log(number(10));
