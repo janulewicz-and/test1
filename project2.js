83aafa897cf1eedef54eceb2d4287b6ef50fb9b0
Andrzej_Janulewicz <and.yanulevich@gmail.com>
2025-03-24 13:45:26 +0100

    name corrected

--- project2.js
+++ project2.js
@@ -1,24 +1,24 @@
-function word(a, b){
+function lettersCheck(a, b){
     if(b.length > a.length){
          return false;
     }
     for(let i = b.length - 1; i >= 0; i--){
         if (a[a.length - b.length + i] !== b[i]){
-        return(false);
+        return false ;
         }
 }
 return true;
 }
 
-function stair(p){
+function MakeChair(p){
     let result = '';
     for(let i = 1; i <= p; i++){
         for(let j = 1; j <= i; j++){
             result += j + ' ';
         }
-    result += '\n';
-}
-return result;
+        result += '\n';
+    }   
+    return result;
 }
 
 console.log(stair(10));
