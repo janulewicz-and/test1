afbad745b333c35ca3d2535a89aa665a66c8f3b2
Andrzej_Janulewicz <and.yanulevich@gmail.com>
2025-03-28 12:09:24 +0100

    new function 1.23

--- project2.js
+++ project2.js
@@ -94,19 +94,28 @@ function repeat(a,b){
 }
 
 function substring(a, b, c){
-    if(a === b){
+    if(typeof a !== 'string'){
+        return false;
+    }
+    if(b === c){
         return '';
-    } else if(b === undefined){
+    }
+    if(b === undefined){
         b = a.length;
-    } else if(isNaN(c) || c <= 0){
+    } 
+    if(isNaN(c) || c <= 0){
         c = 0;
-    } else if(isNaN(b) || b <= 0){
+    }
+    if(isNaN(b) || b <= 0){
         b = 0
-    } else if( a.length < b){
+    }
+    if( a.length < b){
         b = a.length;
-    } else if(a.length < c){
+    }
+    if(a.length < c){
         c = a.length;
-    } else if(b > c){
+    }
+    if(b > c){
         let b1 = b;
         b = c;
         c = b1;
