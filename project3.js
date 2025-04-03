159a3c27fa81a597fef0ed8fdb2cb38ff4e53b71
Andrzej_Janulewicz <and.yanulevich@gmail.com>
2025-04-03 11:03:51 +0200

    new function 1.25

--- project3.js
+++ project3.js
@@ -1,56 +1,10 @@
-function trimStart(a){
-    result= "";
-    let i = 0;
-    for(; i < a.length; i++){
-        if(a[i] !== ' '){
-            break;
-        }
+function prepend(arr, ...items){
+    let result;
+    for(let i = 0; i < items.length; i++){
+        let j = items[i];
+        arr.unshift(j);
     }
-    for(let j = i; j < a.length; j++){
-        result += a[j];
-    }
-    return result;
+    return arr
 }
-
-function trimEnd(a){
-    let result= "";
-    let i = a.length - 1;
-    for(; i > 0; i--){
-        if(a[i] !== ' '){
-            break;
-        }
-    }
-    for(let j = 0; j <= i; j++){
-        result += a[j];
-    }
-    return result;
-}
-
-function trim(a){
-    let result1 = "";
-    let result = '';
-    let i = 0;
-    for(; i < a.length; i++){
-        if(a[i] !== ' '){
-            break;
-        }
-    }
-    for(let j = i; j < a.length; j++){
-        result1 += a[j];
-    }
-
-    let y = a.length - 1;
-    for(; y > 0; y--){
-        if(a[y] !== ' '){
-            break;
-        }
-    }
-    for(let u = i; u <= y; u++){
-        result += a[u];
-    }
-    return result;
-}
-
-
-
-console.log(trim('  квадроцикл би би би    '));
+let massive = ["js", "react"]
+console.log(prepend( massive, 'cat', 'dog', 'mouse', 'bird'))
